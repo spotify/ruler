7469275cf5b4f8a16e4e751276465f726403b1af
Simon Schiller <sschiller@spotify.com>
2021-07-30 20:43:51 +0200

    Automatically promote Sonatype releases

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import io.github.gradlenexus.publishplugin.NexusPublishExtension
+
 buildscript {
     repositories {
         mavenLocal {
             content {
-                includeGroup("com.spotify.ruler") // Only load Ruler plugin from local Maven, for the sample project
+                // Only load Ruler plugin from local Maven (required for the sample project)
+                includeGroup(RULER_PLUGIN_GROUP)
             }
         }
         google()
@@ -15,6 +18,7 @@ buildscript {
         classpath(Dependencies.KOTLINX_SERIALIZATION_GRADLE_PLUGIN)
         classpath(Dependencies.KOTLIN_REACT_FUNCTION_GRADLE_PLUGIN)
         classpath(Dependencies.DETEKT_GRADLE_PLUGIN)
+        classpath(Dependencies.NEXUS_PUBLISH_GRADLE_PLUGIN)
 
         if (!properties.containsKey("withoutSample")) {
             classpath(Dependencies.RULER_GRADLE_PLUGIN)
@@ -22,9 +26,23 @@ buildscript {
     }
 }
 
+apply(plugin = "io.github.gradle-nexus.publish-plugin")
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+group = RULER_PLUGIN_GROUP
+version = RULER_PLUGIN_VERSION
+
+extensions.configure(NexusPublishExtension::class) {
+    repositories {
+        sonatype {
+            username.set(System.getenv(ENV_SONATYPE_USERNAME))
+            password.set(System.getenv(ENV_SONATYPE_PASSWORD))
+        }
     }
 }

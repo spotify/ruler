9099080161fc085ed93c3e3872bf9c055fa04d16
Simon Schiller <sschiller@spotify.com>
2021-08-14 18:15:36 +0200

    Apply detekt plugin to sample project

--- sample/app/build.gradle.kts
+++ sample/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("io.gitlab.arturbosch.detekt")
     id("com.spotify.ruler")
 }
 
--- sample/lib/build.gradle.kts
+++ sample/lib/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
+    id("io.gitlab.arturbosch.detekt")
 }
 
 android {

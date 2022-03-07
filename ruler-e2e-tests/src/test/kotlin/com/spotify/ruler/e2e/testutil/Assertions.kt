e38839593002afba771a0fe39832251eed0a4754
Simon Schiller <sschiller@spotify.com>
2022-03-07 12:49:55 +0100

    Add E2E tests that verify the reports of the sample project

---
+++ ruler-e2e-tests/src/test/kotlin/com/spotify/ruler/e2e/testutil/Assertions.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2021 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.spotify.ruler.e2e.testutil
+
+import com.google.common.truth.Correspondence
+import com.spotify.ruler.models.AppFile
+import com.spotify.ruler.models.FileType
+
+data class FileMatcher(val name: String, val type: FileType, val owner: String)
+
+object Correspondence {
+    fun file() = Correspondence.from(::compareFiles, "matches")
+
+    // Makes it possible to assert files without matching download/install size
+    private fun compareFiles(actual: AppFile?, expected: FileMatcher?): Boolean = when {
+        actual == null -> expected == null
+        expected == null -> false
+        else -> actual.name == expected.name && actual.type == expected.type && actual.owner == expected.owner
+    }
+}
---
+++ ruler-e2e-tests/src/test/kotlin/com/spotify/ruler/e2e/testutil/Parsing.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2021 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.spotify.ruler.e2e.testutil
+
+import com.spotify.ruler.models.AppReport
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.nio.file.Paths
+import kotlin.io.path.readText
+
+/** Parses and returns the sample project report of the given [variant]. */
+fun parseReport(variant: String): AppReport {
+    val path = Paths.get("..", "sample", "app", "build", "reports", "ruler", variant, "report.json")
+    return Json.decodeFromString(path.readText())
+}

e2551d1a1b8769d1d089108a77aee71646b63908
Nathan Brizendine <nathanb@spotify.com>
2023-02-01 11:15:20 +0100

    Fix Selenium connection issues

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -80,8 +80,8 @@ object Dependencies {
         const val KOTLIN_REACT_ROUTER = "6.3.0-pre.343" // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-react-router-dom
         const val KOTLIN_JS_EXTENSIONS = "1.0.1-pre.343" // https://mvnrepository.com/artifact/org.jetbrains.kotlin-wrappers/kotlin-extensions
 
-        const val SELENIUM_WEBDRIVER = "4.3.0" // https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java
-        const val SELENIUM_WEBDRIVER_MANAGER = "5.2.1" // https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager
+        const val SELENIUM_WEBDRIVER = "4.8.1" // https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java
+        const val SELENIUM_WEBDRIVER_MANAGER = "5.3.2" // https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager
 
         const val REACT = "18.2.0" // https://www.npmjs.com/package/react
         const val BOOTSTRAP = "5.1.3" // https://www.npmjs.com/package/bootstrap

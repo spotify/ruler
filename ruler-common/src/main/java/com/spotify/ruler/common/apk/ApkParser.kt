aee7a421d2f482394deb1f6b25d83e628eefd46e
viktorp <viktorp@spotify.com>
2023-07-31 13:36:18 +0200

    Parse native libraries in ApkParser

--- ruler-common/src/main/java/com/spotify/ruler/common/apk/ApkParser.kt
+++ ruler-common/src/main/java/com/spotify/ruler/common/apk/ApkParser.kt
@@ -18,11 +18,12 @@ package com.spotify.ruler.common.apk
 
 import com.android.tools.apk.analyzer.ApkSizeCalculator
 import com.android.tools.apk.analyzer.dex.DexFiles
+import com.spotify.ruler.common.bloaty.Bloaty
 import java.io.File
 import java.util.zip.ZipFile
 
 /** Responsible for parsing and extracting entries from APK files. */
-class ApkParser {
+class ApkParser(private val unstrippedNativeLibraryPaths: List<File> = emptyList()) {
 
     /** Parses and returns the list of entries contained in the given [apkFile]. */
     fun parse(apkFile: File) : List<ApkEntry> {
@@ -41,6 +42,19 @@ class ApkParser {
                     isDexEntry(name) -> {
                         val bytes = zipFile.getInputStream(zipEntry).readBytes()
                         ApkEntry.Dex(name, downloadSize, installSize, parseDexEntry(bytes))
+                    }
+                    isNativeLibraryEntry(name) -> {
+                        val bytes = zipFile.getInputStream(zipEntry).readBytes()
+                        val native = ApkEntry.NativeLibrary(
+                            name,
+                            downloadSize,
+                            installSize,
+                            Bloaty.parseNativeLibraryEntry(
+                                bytes,
+                                debugFileForNativeLibrary(entryName = name)
+                            )
+                        )
+                        native
                     }
                     // When build from bazel resources coming from
                     // kt_android_library rule starts with /lib/res and are not attributed properly.
@@ -66,5 +80,18 @@ class ApkParser {
     /** Checks if a certain [entryName] represents a DEX entry. */
     private fun isDexEntry(entryName: String): Boolean {
         return entryName.endsWith(".dex", ignoreCase = true)
+    }
+
+    /** Checks if a certain [entryName] represents a native library entry. */
+    private fun isNativeLibraryEntry(entryName: String): Boolean {
+        return entryName.endsWith(".so", ignoreCase = true)
+    }
+
+    /** Get the file containing the Unstripped file names to properly parse the native library. */
+    private fun debugFileForNativeLibrary(entryName: String): File? {
+        val entryFileName = File(entryName).nameWithoutExtension
+        return unstrippedNativeLibraryPaths.find {
+            it.name.contains(entryFileName)
+        }
     }
 }
